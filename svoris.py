80225582b8cd7b2f673a3757217ef6faa1ea9ada
Valdas1975 <k.valdas@protonmail.com>
2020-12-22 09:47:16 +0200

    svoris

---
+++ svoris.py
@@ -0,0 +1,24 @@
+print("Svaru konvertavimas ji kilogramus.")
+
+pounds = eval(input("Svorio konvertavimas SVARAI= "))
+
+print()
+
+kilograms = pounds * 0.454
+
+print()
+
+print("svorys kilogramais=", kilograms, "kg")
+
+string = 'QWERTYUIOPLKJHGFDSAZXCVBNM'
+print(string.lower())
+
+pounds = 10
+
+kg = pounds / 2.200462262
+
+print(f"pounds({pounds}) => kg({kg})")
+
+kg =10
+
+pounds = kg * 2.20462262
